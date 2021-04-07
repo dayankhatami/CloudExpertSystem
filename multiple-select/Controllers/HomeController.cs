3c907ac5ba2c0c5b090bc90855270f40f68a01fd
dayankhatami_ <dayankhatamia@gmail.com>
2021-04-07 19:12:51 +0700

    Push first

---
+++ multiple-select/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace multiple_select.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddCourse(ViewModels.AddAtribute add)
+        {
+            if (add.Tags != null)
+            {
+                foreach (var item in add.Tags)
+                {
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ multiple-select/ViewModels/AddAtribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace multiple_select.ViewModels
+{
+    public class AddAtribute
+    {
+        public ICollection<string> Tags { get; set; }
+    }
+}
